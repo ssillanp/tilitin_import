d4e117e3723a482504c7b051f5abec066dd2c783
Sami <60390828+ssillanp@users.noreply.github.com>
2021-04-22 08:48:41 +0300

    Class improvement

--- bankinfo.py
+++ bankinfo.py
@@ -1,42 +1,43 @@
 class bankinfo:
     """Luokka pankkikohtaisille asetuksille"""
-    def __init__(self):
-        self.op = {
-            'delimiter': ';',
-            'timeformat': '%d.%m.%Y',
-            'datecol': 0,
-            'sumcol': 2,
-            'descol': 6
-        }
+    def __init__(self, bank):
+        self.set_values(self, bank)
+        
+    def set_values(bank):
+        if bank == 'op':
+            self.delimiter =  ';'
+            self.timeformat = '%d.%m.%Y'
+            self.datecol = 0
+            self.sumcol = 2
+            self.descol = 6        
+        elif bank == 'danske':
+            self.delimiter = ';'
+            self.timeformat = '%d.%m.%Y'
+            self.datecol = 0
+            self.sumcol = 2
+            self.descol = 1
+#         elif bank == 'nordea':
+#             self.delimiter = ';'
+#             self.timeformat = '%d.%m.%Y'
+#             self.datecol = 0
+#             self.sumcol = 1
+#             self.descol = 5
+#         else:
+#             user_defined()
 
-        self.danske = {
-            'delimiter': ',',
-            'timeformat': '%d.%m.%Y',
-            'datecol': 0,
-            'sumcol': 2,
-            'descol': 1
-        }
 
-        # self.nordea = {
-        #     'delimiter': ';',
-        #     'timeformat': '%d.%m.%Y',
-        #     'datecol': 0,
-        #     'sumcol': 1,
-        #     'descol': 5
-        # }
-
-    def user_defined(self):
-        """Käyttäjän vapaasti määriteltävät pankki-csv asetukset"""
-        delimiter = input("Kenttäerotin [,]:") or ","
-        timeformat = input("Timeformat [%d.%m.%Y]:") or "%d.%m.%Y"
-        datecolumn = input("Päivämäärän sarake [0]:") or 0
-        sumcolumn = input("Summan sarake [2]:") or 2
-        desc_column = input("Kuvauksen sarake [1]:") or 1
-        self.user = {
-            'delimiter': delimiter,
-            'timeformat': timeformat,
-            'datecol': int(datecolumn),
-            'sumcol': int(sumcolumn),
-            'descol': int(desc_column)
-        }
-        return self.user
+#     def user_defined(self):
+#         """Käyttäjän vapaasti määriteltävät pankki-csv asetukset"""
+#         delimiter = input("Kenttäerotin [,]:") or ","
+#         timeformat = input("Timeformat [%d.%m.%Y]:") or "%d.%m.%Y"
+#         datecolumn = input("Päivämäärän sarake [0]:") or 0
+#         sumcolumn = input("Summan sarake [2]:") or 2
+#         desc_column = input("Kuvauksen sarake [1]:") or 1
+#         self.user = {
+#             'delimiter': delimiter,
+#             'timeformat': timeformat,
+#             'datecol': int(datecolumn),
+#             'sumcol': int(sumcolumn),
+#             'descol': int(desc_column)
+#         }
+#         return self.user
