ac5374e4217e4c8d1b03aa003eba345c2f454a28
sami <sami.sillanpaa@iki.fi>
2021-04-22 21:19:26 +0300

    class bankinfo.py changed

--- bankinfo.py
+++ bankinfo.py
@@ -26,18 +26,16 @@ class bankinfo:
 #             user_defined()
 
 
-#     def user_defined(self):
-#         """Käyttäjän vapaasti määriteltävät pankki-csv asetukset"""
-#         delimiter = input("Kenttäerotin [,]:") or ","
-#         timeformat = input("Timeformat [%d.%m.%Y]:") or "%d.%m.%Y"
-#         datecolumn = input("Päivämäärän sarake [0]:") or 0
-#         sumcolumn = input("Summan sarake [2]:") or 2
-#         desc_column = input("Kuvauksen sarake [1]:") or 1
-#         self.user = {
-#             'delimiter': delimiter,
-#             'timeformat': timeformat,
-#             'datecol': int(datecolumn),
-#             'sumcol': int(sumcolumn),
-#             'descol': int(desc_column)
-#         }
-#         return self.user
+    def user_defined(self):
+        """Käyttäjän vapaasti määriteltävät pankki-csv asetukset"""
+        delimiter = input("Kenttäerotin [,]:") or ","
+        timeformat = input("Timeformat [%d.%m.%Y]:") or "%d.%m.%Y"
+        datecolumn = input("Päivämäärän sarake [0]:") or 0
+        sumcolumn = input("Summan sarake [2]:") or 2
+        desc_column = input("Kuvauksen sarake [1]:") or 1
+        self.delimiter =  delimiter,
+        self.timeformat = timeformat,
+        self.datecol = int(datecolumn),
+        self.sumcol =int(sumcolumn),
+        self.descol =int(desc_column)
+        return None
